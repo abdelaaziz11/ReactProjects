ff63f87ad5e766ee7e49a59bc744acee5f717c9a
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-10-20 20:49:01 +0100

    best way to styling on react js

--- src/Component/Routers/About.jsx
+++ src/Component/Routers/About.jsx
@@ -1,10 +1,19 @@
 import Header from '../Header';
 
 const About = () => {
+  const aboutStyle = {
+    color: 'white',
+    backgroundColor: 'black',
+    borderRadius: '2rem',
+    padding: '10px'
+  }
+  const h1Style = {
+    marginLeft: '20px'
+  }
     return (
       <>
         <Header></Header>
-        <div className="container mx-75 mt-5"><h1>About Page</h1></div>
+        <div style={aboutStyle} className="container mx-75 mt-5"><h1 style={h1Style} >About Page</h1></div>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Rooters from './Component/Routes2/Routes2'
+import Rooters from './Component/Routers/Routes'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
