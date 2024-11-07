a95e40a6b3b8d78d156dd847fd2ae3864452acfc
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-11-07 16:25:04 +0100

    Second way in Use useMemo to cache calculated value in form but we use React.memo() function this case

--- src/Component/UseMemo/UseMemo.jsx
+++ src/Component/UseMemo/UseMemo.jsx
@@ -2,39 +2,36 @@ import React, { useMemo, useRef, useState } from 'react';
 
 const expensiveCalcul = (value) => {
     for(let i = 0; i < 1000; i++) {
-        value++
+        value++;
     }
-    return value
-}
+    return value;
+};
 
 const UseMemo = () => {
-    const [n, setN] = useState(0)
-    const text = useRef()
-    const startDate = performance.now()
-    let endDate = null
+    const [n, setN] = useState(0);
+    const text = useRef();
+    const startDate = performance.now();
+    let endDate = null;
 
-    
     const calculate = useMemo(() => {
-        const value = (expensiveCalcul(n))
-        endDate = performance.now()
-        console.log((endDate - startDate) + 'ms.')
-        console.log(value)
-        return value
-    }, [n]);
-    
+        const value = expensiveCalcul(n);
+        endDate = performance.now();
+        console.log((endDate - startDate) + 'ms.');
+        console.log(value);
+        return value;
+    }, [n]); // useMemo memoizes the calculated value based on 'n'
+
     const handleClick = () => {
-        setN(parseInt(text.current.value))
-    }
+        setN(parseInt(text.current.value));
+    };
 
     return (
         <div className="container m-4">
-            <input defaultValue={0} type="text" ref={text}/>
+            <input defaultValue={0} type="text" ref={text} />
             <button className="btn btn-primary" onClick={handleClick}>Re-calculate</button>
-            
             <h1>Value : {calculate}</h1>
-            
         </div>
     );
-}
+};
 
-export default UseMemo;
+export default React.memo(UseMemo);
