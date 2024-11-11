22ee85668f30fdbfed056c806ebfa6f121f1cb66
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-11-11 21:39:27 +0100

    Table of users

--- src/Component/Users/UsersTable.jsx
+++ src/Component/Users/UsersTable.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-function UsersTable(props) {
+
+function UsersTable({users}) {
     return (
-        <div>
-            
+        <div className="container">
+            <h1 className="mt-5">Users</h1>
+
+            <table className="table table-striped">
+            <thead>
+                    <tr>
+                        <th>#ID</th>
+                        <th>Name</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        (users.length > 0) ? users.map((user, key) => 
+                        <tr key={key}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.country}</td>
+                        </tr>) 
+                        : <tr><td colSpan={3} align={'center'}> No Items</td></tr>
+                   }
+                </tbody>
+            </table>
         </div>
     );
 }
