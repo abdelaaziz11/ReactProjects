17d0ce9f22b371eae92a1110acd209698632363a
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-11-11 21:38:43 +0100

    App file of users

--- src/Component/Users/UsersApp.jsx
+++ src/Component/Users/UsersApp.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UsersTable from './UsersTable';
 
-function UsersApp(props) {
+const INITIAL_DATA = [
+    {
+        id: 1,
+        name: 'Abdelaaziz',
+        country: 'Morocco'
+    },
+    {
+        id: 2,
+        name: 'Khouda',
+        country: 'French'
+    }
+]
+
+const UsersApp = () => {
+    const [users, setUsers] = useState(INITIAL_DATA)
     return (
-        <div>
-            
-        </div>
+            <UsersTable users={users}/>
     );
 }
 
