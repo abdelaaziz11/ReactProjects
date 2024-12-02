8763d75639b0c4149a1916f7c001bc320b424660
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-12-02 15:08:09 +0100

    Use HOC (Higher Order Component) to create an increment onClick and onHover

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './Component/ReduxToolkit/App';
 import store from "./Component/Redux/store"
 import {Provider} from 'react-redux'
+import WithCounter from './Component/Hoc/WithCounter';
+import CounterClick from './Component/Hoc/CounterClick';
+import CounterHover from './Component/Hoc/CounterHover';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Provider store={store}>
-      <div className="container justify-content-center align-items-center d-flex">
-        <App/>
-      </div>
+        <CounterClick/>
+        <CounterHover title={'counter'}  />
     </Provider>
   </>
 );
